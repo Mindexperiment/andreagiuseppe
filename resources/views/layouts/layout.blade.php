d42c6f2ba22de15ae5387dabf3a552ff5ce244dd
Mindexperiment <andrewloyd@libero.it>
2019-07-08 17:08:01 +0200

    add canonical link for website

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,6 +7,8 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
+  <link href="https://andreagiuseppe.com" rel="canonical" />
+
   <!-- Stylesheets -->
   <link rel="stylesheet" href="{{ asset( 'css/app.css' ) }}" />
   @stack( 'stylesheets' )
