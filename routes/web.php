f193f46128d2fff003667a4a2b4f9c209feddf2b
Mindexperiment <andrewloyd@libero.it>
2019-07-23 07:19:37 +0200

    open routes for articles

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Articles
+Route::post( '/articles/store', 'ArticlesController@store' )->name( 'articles.store' );
+Route::post( '/articles/{article}/publish', 'ArticlesController@publish' )->name( 'articles.publish' );
+Route::delete( '/articles/{article}', 'ArticlesController@destroy' )->name( 'articles.destroy' );
+Route::delete( '/articles/{article}/publish', 'ArticlesController@unpublish' )->name( 'articles.unpublish' );
+Route::get( '/articles', 'ArticlesController@index' )->name( 'articles.index' );
+Route::get( '/articles/create', 'ArticlesController@create' )->name( 'articles.create' );
