e951d79bfbf858acaa8a206b708ccab43819fbce
Mindexperiment <andrewloyd@libero.it>
2019-08-04 22:55:23 +0200

    add header menu section

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,9 @@
 <header>
- <div>{{ config( 'app.name' ) }}</div>
+ <div>
+  <div><a href="{{ route( 'website.homepage' ) }}">{{ config( 'app.name' ) }}</a></div>
+
+  <div>
+   <span><a href="{{ route( 'articles.speeches' ) }}">{{ __( 'Articoli' ) }}</a></span>
+  </div>
+ </div>
 </header>
