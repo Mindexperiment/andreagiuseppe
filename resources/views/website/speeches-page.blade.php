ee9df07f24568d1d370e2c7219b196a669539843
Mindexperiment <andrewloyd@libero.it>
2019-08-05 11:28:47 +0200

    add website speeches-page

---
+++ resources/views/website/speeches-page.blade.php
@@ -0,0 +1,27 @@
+@extends( 'layouts.page' )
+
+@section( 'page' )
+<section>
+ <header>
+  <h2>{{ __( 'Articoli' ) }}</h2>
+
+  <p>{{ _( 'Alcune delle mie discussioni preferite da sviluppatore web' ) }}</p>
+ </header>
+
+ <div>
+  @forelse( $articles as $article )
+   <div>
+    <p>
+     <a href="{{ route( 'website.speech', $article ) }}">{{ $article->title }}</a>
+     <span>{{ $article->updated_at->diffForHumans() }}</span>
+    </p>
+   </div>
+
+  @empty
+   <p>{{ __( 'Nessun articolo' ) }}</p>
+
+  @endforelse
+ </div>
+</section>
+
+@endsection
