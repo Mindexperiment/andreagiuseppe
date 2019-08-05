3a87a5001ace00f765d64ab1dec0b1ac7d318787
Mindexperiment <andrewloyd@libero.it>
2019-08-05 11:10:25 +0200

    open routes for article edit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 // Articles
 Route::post( '/articles/store', 'ArticlesController@store' )->name( 'articles.store' );
 Route::post( '/articles/{article}/publish', 'ArticlesController@publish' )->name( 'articles.publish' );
+Route::patch( '/articles/{article}', 'ArticlesController@update' )->name( 'articles.update' );
 Route::delete( '/articles/{article}', 'ArticlesController@destroy' )->name( 'articles.destroy' );
 Route::delete( '/articles/{article}/publish', 'ArticlesController@unpublish' )->name( 'articles.unpublish' );
 Route::get( '/articles', 'ArticlesController@index' )->name( 'articles.index' );
 Route::get( '/articles/create', 'ArticlesController@create' )->name( 'articles.create' );
+Route::get( '/articles/{article}/edit', 'ArticlesController@edit' )->name( 'articles.edit' );
