5cde9ed878cc81f0dcb084c9c6ce20cfa1dcd8bd
Mindexperiment <andrewloyd@libero.it>
2019-08-05 14:55:27 +0200

    update webpack.mix with js and images

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const tailwindcss = require('tailwindcss');
  */
 mix.copyDirectory('resources/fonts', 'public/resources/fonts');
 
+mix.js('resources/js/app.js', 'public/js');
 mix.sass('resources/sass/app.scss', 'public/css')
  .options({
   processCssUrls: false,
